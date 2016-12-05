f1a4b73044a75bae2e2b683acb61da3487684c83
Kamil Stepien <kamilste@hotmail.com>
2016-12-05 12:42:11 +0000

    Update FullScreen.js

--- FullScreen.js
+++ FullScreen.js
@@ -1,13 +1,9 @@
-	
 function goFullscreen(id) {
 	var element = document.getElementById(id); // Get the element that we want to take into fullscreen mode
 	if (element.mozRequestFullScreen) {
 	element.mozRequestFullScreen();} // Go into fullscreen mode in Firefox
 		else if (element.webkitRequestFullScreen) {
 		element.webkitRequestFullScreen();}} //Go into fullscreen mode in Chrome and Safari
-		
-
-		
 			function exitFullscreen() {
   				if(document.exitFullscreen) {
      				document.exitFullscreen();} 
@@ -16,4 +12,3 @@ function goFullscreen(id) {
 				else if(document.webkitExitFullscreen) {
 					document.webkitExitFullscreen();}}
 				exitFullscreen(); // Cancel fullscreen
-		
