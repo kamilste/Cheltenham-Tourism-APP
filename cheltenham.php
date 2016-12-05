640a0a9d1395b5442f96dd0b6ca0cf190a6b9978
Kamil Stepien <kamilste@hotmail.com>
2016-12-05 12:44:48 +0000

    Update cheltenham.php

--- cheltenham.php
+++ cheltenham.php
@@ -1,11 +1,9 @@
-
 <?php
 
-
 $servername = "localhost";
-$username = "s1206852";
-$password = "Afghan55";
-$dbname = "s1206852_Businesses";
+$username = "*****";
+$password = "*****";
+$dbname = "*****_Businesses";
 
 function getEventList(){ 
     
@@ -39,8 +37,3 @@ function getEventList(){
 getEventList();
 
 ?>
-
-
-
-
-
